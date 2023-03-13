e50b4d5cf1b41d5878d6958e7a2b5f4e9d8934bf
Rohit9252 <rohitbatra0165@gmail.com>
2023-03-13 14:02:39 +0530

    clone-done

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            '1810597ddemsh5f5288c59120a68p14ff2ajsna9a74f604df5',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
